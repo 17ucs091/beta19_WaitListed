f02697530be3870c3ca6669020b0c1aca4bcfaf7
iamakshatjain <jain99akshat@gmail.com>
2020-10-31 17:04:40 +0530

    feat: detect tab switching

--- proctorx/src/components/App.js
+++ proctorx/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -14,10 +14,34 @@ const useStyles = makeStyles((theme) => ({
 const App = () => {
   const classes = useStyles();
 
+  useEffect(() => {
+    // Prevent Ctrl+S, Ctrl+C & Ctrl+V
+    document.onkeydown = function (e) {
+      e = e || window.event; //Get event
+      if (e.ctrlKey) {
+        var c = e.which || e.keyCode; //Get key code
+        if ([83, 67, 86].includes(c)) {
+          console.log('Ctrl + S, Ctrl+C, Ctrl+V not allowed');
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }
+    };
+
+    // detect tab switching
+    document.addEventListener('visibilitychange', (event) => {
+      if (document.visibilityState === 'visible') {
+        console.log('tab is active');
+      } else {
+        console.log('tab is inactive');
+      }
+    });
+  });
+
   return (
     <Grid container>
       <Grid item xs={6}>
-        <Paper className={classes.fullview}>xs=6</Paper>
+        <Paper className={classes.fullview}>xs = 6</Paper>
       </Grid>
       <Grid item xs={6}>
         <Paper className={classes.fullview}>
