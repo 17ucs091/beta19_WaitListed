56eac17a9f957c6b4efe1103f79e5cc8aa0c30b1
Anubhav Natani <30464412+ANUBHAVNATANI@users.noreply.github.com>
2020-11-01 16:23:23 +0530

    Update Camera.js

--- proctorx/src/components/Camera.js
+++ proctorx/src/components/Camera.js
@@ -41,12 +41,12 @@ const Camera = () => {
       img = img.toFloat().div(tf.scalar(255));
       //console.log(img.print());
       // img = tf.browser.fromPixels({data:img,width:224,height:224});
-      //img = tf.cast(img, 'float32');
-      //img = tf.expandDims(img);
+      img = tf.cast(img, 'float32');
+      img = tf.expandDims(img);
 
       
       //console.log(img);
-      let arr = Float32Array.from([[[[1,2,3],[1,2,1]]]]);
+      //let arr = Float32Array.from([[[[1,2,3],[1,2,1]]]]);
       //arr = tf.tensor4d(arr, [1, 224, 224, 3], 'float32');
       // console.log(await img.array());
       // img.print();
